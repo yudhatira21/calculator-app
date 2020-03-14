0e8e2e225615a10552f02b90191dc41710345683
yudhatira21 <katsunan2001@gmail.com>
2020-03-14 05:55:43 +0100

    menambahkan funsi

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,13 @@
 {
     static void Main(string[] args)
     {
+		int a=10;
+		int b=6;
 
+		Console.WriteLine("Hasil Penambahan: {0}+{1}={2}"), a, b, Penambahan(a,b));
+        Console.WriteLine("Hasil Penambahan: {0}+{1}={2}"), a, b, Pengurangan(a,b));
+        Console.WriteLine("\nTekan key untuk keluar");
+        Console.ReadKey();
     }
 
 	    static int Penambahan(int a, int b)
